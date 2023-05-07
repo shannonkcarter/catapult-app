9cc69a56a90b7655fb36f8783f683b800342e3d0
Shannon Carter <skcarter25@gmail.com>
2023-05-07 12:04:17 -0500

    add some styling; stand up blank app

--- read-data.R
+++ read-data.R
@@ -4,6 +4,7 @@
   library(here)
   library(shiny)
   library(googlesheets4)
+  library(shinyjs)
 }
 
 workout_data_raw = read_sheet("https://docs.google.com/spreadsheets/d/10zq2s20xFFlSFSB08erJP7Z5go-EGD6henb4ZLj-okw/edit#gid=1348903352")
--- ui.R
+++ ui.R
@@ -1,33 +1,38 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+source(here::here("read-data.R"))
 
-library(shiny)
-
-# Define UI for application that draws a histogram
-fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
-)
+function(request){
+  shinyUI(
+    tagList(
+      shinyjs::useShinyjs(),
+      tags$head(
+        HTML("<title>Team Datapult</title>"),
+        tags$script(src="https://kit.fontawesome.com/8abb217f2e.js", crossorigin="anonymous"),
+        tags$link(rel="shortcut icon", href="favicon.png"),
+        tags$link(rel = "stylesheet", type = "text/css", href = "custom.css"),
+        tags$script(HTML("$('body').addClass('fixed');")),
+        tags$link(rel="stylesheet", href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&display=swap"),
+        tags$link(rel="stylesheet", href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap"),
+        tags$link(rel="stylesheet", href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap"),
+  
+        
+      ),
+      
+      navbarPage(
+        id = "tab_being_displayed",
+        selected = "home",
+        collapsible = T,
+        position = c("fixed-top"),
+        title = a(img(src="catapult.jpeg", class="aht-logo", height = 70), type="link", href="https://teamcatapult.org/"),
+      
+        tabPanel(title = "Home", value = "home",
+                 br(),
+                 br(),
+                 br(),
+                 br(),
+                 p("hii")
+        ) # close home
+        
+        ) # close navbarPage
+    ) # close tagList
+  ) # close whole UI
+}
