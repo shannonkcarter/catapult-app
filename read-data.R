c88b781b961dde7fada070a507f640df056ad436
Shannon Carter <skcarter25@gmail.com>
2023-05-07 11:40:24 -0500

    initialize catapult repo

---
+++ read-data.R
@@ -0,0 +1,9 @@
+{
+  library(tidyverse)
+  library(janitor)
+  library(here)
+  library(shiny)
+  library(googlesheets4)
+}
+
+workout_data_raw = read_sheet("https://docs.google.com/spreadsheets/d/10zq2s20xFFlSFSB08erJP7Z5go-EGD6henb4ZLj-okw/edit#gid=1348903352")
